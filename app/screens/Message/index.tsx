f243c1dff6f6f360ec330376fc93c8643910ee3e
AnBuiii <builehoaian2002@gmail.com>
2023-05-29 17:33:44 +0700

    add firebase

--- app/screens/Message/index.tsx
+++ app/screens/Message/index.tsx
@@ -26,50 +26,67 @@ import { EvilIcons, FontAwesome, FontAwesome5, Ionicons, Entypo, MaterialCommuni
 import { Animated, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native';
 import { FlatList, GestureHandlerRootView, RectButton } from 'react-native-gesture-handler';
 
+
 const userList = [
     {
         name: 'Thang abc dgac ',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: true,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: true,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: true,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: true,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: true,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
     {
         name: 'Thang',
         avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
+    },
+    {
+        name: 'Thang',
+        avt: 'https://images.unsplash.com/photo-1682687220801-eef408f95d71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        isRead: false,
     },
 ];
 
@@ -98,7 +115,7 @@ const rightSwipeActions = () => {
     );
 };
 
-const ListItem = (item: { name: string; avt: string; onPress?: () => void }) => (
+const ListItem = (item: { name: string; avt: string; isRead: boolean; onPress?: () => void }) => (
     <GestureHandlerRootView>
         <Swipeable renderRightActions={rightSwipeActions} renderLeftActions={() => <View />}>
             <Pressable onPress={item.onPress}>
@@ -109,11 +126,30 @@ const ListItem = (item: { name: string; avt: string; onPress?: () => void }) => 
                         style={{ width: 64, height: 64 }}
                         borderRadius={100}
                     ></Image>
-                    <VStack flex={1} space={2} justifyContent="center">
-                        <Text bold fontSize="md">
-                            {item.name}
-                        </Text>
-                        <Text isTruncated color="gray.500">
+                    <VStack flex={1} space={0} justifyContent="center">
+                        <HStack alignItems={'center'} space={2}>
+                            <Text
+                                bold={item.isRead ? false : true}
+                                fontSize="md"
+                                color={item.isRead ? 'gray.500' : 'black'}
+                            >
+                                {item.name}
+                            </Text>
+                            {!item.isRead ? (
+                                <Box
+                                    borderWidth={2}
+                                    borderColor="white"
+                                    borderRadius={100}
+                                    width={4}
+                                    height={4}
+                                    bg="blue.500"
+                                ></Box>
+                            ) : (
+                                <></>
+                            )}
+                            <Box></Box>
+                        </HStack>
+                        <Text isTruncated color={item.isRead ? 'gray.500' : 'black'} bold={item.isRead ? false : true}>
                             {item.avt}
                         </Text>
                     </VStack>
@@ -129,6 +165,8 @@ const ListItem = (item: { name: string; avt: string; onPress?: () => void }) => 
 export const MessageScreen = (props: AppTabsStackScreenProps<AppTabsNavigationKey.Message>) => {
     const { navigation } = props;
     const { colors } = useTheme();
+
+    const [messes, setMesses] = useState(userList)
     useEffect(() => {
         props.navigation.setOptions({
             headerTitle: 'Channels',
@@ -193,7 +231,9 @@ export const MessageScreen = (props: AppTabsStackScreenProps<AppTabsNavigationKe
                             <ListItem
                                 {...item}
                                 key={idx}
-                                onPress={() => navigation.navigate(RootNavigatekey.MessageDetail)}
+                                onPress={() => {
+                                    navigation.navigate(RootNavigatekey.MessageDetail);
+                                }}
                             />
                         ))}
                     </VStack>
